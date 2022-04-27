8828b8bf86313a2dbe01e25124cddb67e3d6c330
jerednav <80506833+jerednav@users.noreply.github.com>
2022-04-26 19:48:23 -0700

    adding name, email and password to Register page using FormRow component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 //use index.js to import these files through one path, instead of adding multiple paths for each components
 
 import Logo from "./Logo";
+import FormRow from "./FormRow";
 
-export { Logo };
+export { Logo, FormRow };
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,57 @@
+import { useState, useEffect } from "react";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+//global context and useNavigate later
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+  //global state and useNavigate
+
+  //will fire if one of the inputs change in the register form
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault(console.log(e.target));
+  };
+  //full-page className is to make the viewing height 100 (vh100)
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* name input */}
+        <FormRow
+          type='text'
+          name='name'
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email input */}
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password input */}
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type='submit' className='btn btn-block'></button>
+      </form>
+    </Wrapper>
+  );
 };
 export default Register;
