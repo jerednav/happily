bff929a2c4c9ad368fccaa77fc47ddc76ca7fcb9
jerednav <80506833+jerednav@users.noreply.github.com>
2022-04-27 13:23:37 -0700

    setting up basic register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,18 @@
-const register = async (req,res) => {
-    res.send('register user')
-}
-const login = async (req,res) => {
-    res.send('login user')
-}
-const updateUser = async (req,res) => {
-    res.send('update user')
-}
+import User from "../models/User.js";
 
-export { register, login, updateUser}
+const register = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({ user });
+  } catch (error) {
+    res.status(500).json({ msg: "there was an error" });
+  }
+};
+const login = async (req, res) => {
+  res.send("login user");
+};
+const updateUser = async (req, res) => {
+  res.send("update user");
+};
+
+export { register, login, updateUser };
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,8 +1,8 @@
 //looking for errors that are happening in our existing routes
 
-const errorHandlerMiddleware = (err, req,res, next) => {
-    console.log(err)
-    res.status(500).json({msg:'there was an error'})
-}
+const errorHandlerMiddleware = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "there was an error" });
+};
 
-export default errorHandlerMiddleware
+export default errorHandlerMiddleware;
