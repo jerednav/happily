cde49a65c13e130e9b290ee6ca403f55f937702e
jerednav <80506833+jerednav@users.noreply.github.com>
2022-04-27 11:21:12 -0700

    created middlewares and env variable

--- Process.md
+++ Process.md
@@ -190,3 +190,29 @@ package.json
 
 "type":"module"
 ```
+
+#### Nodemon and Basic Express Server
+
+```sh
+npm install nodemon --save-dev
+```
+
+```js
+package.json
+"start":"nodemon server"
+```
+
+```sh
+npm install express
+```
+- import express
+
+```js
+import express from 'express'
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Welcome!')
+})
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server is listening on port ${port}...`))
+```
--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log('Server running...');
+import express from 'express'
+const app = express()
+import dotenv from 'dotenv'
+//start dotenv
+dotenv.config()
+
+//middleware
+import errorHandlerMiddleware from './middleware/error-handler.js'
+import notFoundMiddleware from './middleware/not-found.js'
+
+ app.get('/', (req,res)=> {
+     throw new Error('error')
+     res.send('Welcome!')
+ })
+
+ app.use(notFoundMiddleware)
+ app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 8000
+
+app.listen(port,() => {
+    console.log(`Server is listening on port ${port}...`)
+})
