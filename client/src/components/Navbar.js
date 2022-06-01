1f5500806e02afa266a047facced53830bf6dec0
jerednav <80506833+jerednav@users.noreply.github.com>
2022-05-31 18:50:16 -0700

    creating navbar items

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,43 @@
-import Wrapper from '../assets/wrappers/Navbar'
+import Wrapper from "../assets/wrappers/Navbar";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
 
 const Navbar = () => {
   return (
     <Wrapper>
-        <h4>Navbar</h4>
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => console.log("toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          {/* <Logo /> */}
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("show/hide dropdown")}
+          >
+            <FaUserCircle />
+            jered
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button type="button" className="dropdown-btn"
+            onClick={() => console.log("logout user")}
+            >
+            logout 
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
