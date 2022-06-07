9f9e10c04eb5074036f6b2f2c5675acf64cd9e9b
jerednav <80506833+jerednav@users.noreply.github.com>
2022-06-07 16:11:36 -0700

    configured smallsidebar

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -20,7 +20,19 @@ const SmallSidebar = () => {
             <FaTimes />
           </button>
           <header>Logo</header>
-          <div className="nav-links">nav links</div>
+          <div className="nav-links">
+            {links.map((link) => {
+              const {text,path,id,icon} = link
+
+              return(
+                <NavLink to={path} key={id} onClick={toggleSidebar}
+                className={({isActive})=> isActive? 'nav-link active': 'nav-link'}>
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              )
+            })}
+          </div>
         </div>
       </div>
     </Wrapper>
