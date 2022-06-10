832fd21f15a7c3a81d79f462309566513bac3e05
jerednav <80506833+jerednav@users.noreply.github.com>
2022-06-09 23:53:01 -0700

    adding links to bigSidebar

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,11 +1,21 @@
-import Wrapper from "../assets/wrappers/BigSidebar"
+import { useAppContext } from "../context/appContext";
+// import Logo from "./Logo";
+import NavLinks from "./NavLinks";
+import Wrapper from "../assets/wrappers/BigSidebar";
 
 const BigSidebar = () => {
-  return (
-    <Wrapper>
-        <h4>Big Sidebar</h4>
-    </Wrapper>
-  )
-}
+  const { showSidebar, toggleSidebar } = useAppContext();
+  return <Wrapper>
+   <div className={showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'}
+   >
+     <div className='content'>
+       <header>
+         Logo
+       </header>
+       <NavLinks />
+     </div>
+   </div>
+  </Wrapper>;
+};
 
-export default BigSidebar
+export default BigSidebar;
