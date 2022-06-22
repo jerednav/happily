3f3af1ee7d4bd2a5761e27b161f7d820f525d094
jerednav <80506833+jerednav@users.noreply.github.com>
2022-06-21 21:40:57 -0700

    updating modified paths in server

--- models/User.js
+++ models/User.js
@@ -32,8 +32,11 @@ const UserSchema = new mongoose.Schema({
 
 //password hashed when sent to database
 UserSchema.pre("save", async function () {
-  // const salt = await bcrypt.genSalt(10);
-  // this.password = await bcrypt.hash(this.password, salt);
+  // console.log(this.modifiedPaths());
+  // console.log(this.isModified('name'));
+  if (!this.isModified('password')) return
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 //createJWT is just the function name, can be changed
